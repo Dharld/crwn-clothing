ea5752168bc8a098ecb11a1ea5f61b6f5fc169a5
Yann djoumessi <yanndjoumessi@gmail.com>
2021-12-30 00:49:52 +0100

    created the homepage component

---
+++ src/homepage.component.js
@@ -0,0 +1,41 @@
+import React from "react"
+import "./homepage.style.scss"
+
+const Homepage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">HATS</div>
+                    <div className="subtitle"></div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">Jackets</div>
+                    <div className="subtitle"></div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">Sneakers</div>
+                    <div className="subtitle"></div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">Women</div>
+                    <div className="subtitle"></div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">Men</div>
+                    <div className="subtitle"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Homepage
