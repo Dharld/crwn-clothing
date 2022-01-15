1e9356f9da2d6188d648814f5139946520a4b919
Yann djoumessi <yanndjoumessi@gmail.com>
2022-01-15 16:10:44 +0100

    test2

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,17 @@ import rootReducer from "./rootReducer";
 import loggerMiddleware from "redux-logger"
 import { persistStore } from 'redux-persist'
 
+const middlewares = []
+
+if(process.env.NODE_ENV !== "production") {
+    middlewares.push(loggerMiddleware)
+}
+
 export default (preloadedState) => {
 
     let store = configureStore({
         reducer: rootReducer,
-        middleware: [loggerMiddleware],
+        middleware: middlewares,
         preloadedState
     })
 
